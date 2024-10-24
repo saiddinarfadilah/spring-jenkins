b7a562d7237fadf74b8151156c29a3c50257e93d
Said Dinar Fadilah <saiddinarfadilah@gmail.com>
2024-10-24 21:58:09 +0700

    ci(file): add Jenkinsfile.shared.library3

--- Jenkinsfile.shared.library3.groovy
+++ Jenkinsfile.shared.library3.groovy
@@ -14,7 +14,7 @@ pipeline {
         stage('Test') {
             steps {
                 script {
-                    sampleLibrary3.runTests('gradle')
+                    sampleLibrary3.runTests('maven')
                 }
             }
         }
